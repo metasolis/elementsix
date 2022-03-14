7bdb2d4158d518763ed5acd009812ac448c79fd5
witwiki <Vikram.Udyawer@community.isunet.edu>
2022-03-14 14:51:40 +1300

    added setters getters & tasks & compiled

--- contracts/ElementSix.sol
+++ contracts/ElementSix.sol
@@ -5,18 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ElementSix is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private currentTokenId;
-    
-    constructor() ERC721("ElementSix", "E6") {}
-    
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
-        currentTokenId.increment();
-        uint256 newItemId = currentTokenId.current();
-        _safeMint(recipient, newItemId);
-        return newItemId;
-    }
+  using Counters for Counters.Counter;
+
+  Counters.Counter private currentTokenId;
+
+  /// @dev Base token URI used as a prefix by tokenURI().
+  string public baseTokenURI;
+
+  constructor() ERC721("ElementSix", "E6") {
+    baseTokenURI = "";
+  }
+
+  function mintTo(address recipient) public returns (uint256) {
+    currentTokenId.increment();
+    uint256 newItemId = currentTokenId.current();
+    _safeMint(recipient, newItemId);
+    return newItemId;
+  }
+
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public {
+    baseTokenURI = _baseTokenURI;
+  }
 }
