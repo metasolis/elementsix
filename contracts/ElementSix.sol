6782e4b5d60edbdeec37e29cb77ea494cb80882f
witwiki <Vikram.Udyawer@community.isunet.edu>
2022-03-15 15:55:26 +1300

    added etherscanApi ownable.sol and pullpayment.sol

--- contracts/ElementSix.sol
+++ contracts/ElementSix.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ElementSix is ERC721 {
+contract ElementSix is ERC721, PullPayment, Ownable {
   using Counters for Counters.Counter;
+
+  // Constants
+  uint256 public constant TOTAL_SUPPLY = 195; /// Idea for 721, fun with flags collection
+  uint256 public constant MINT_PRICE = 0.08 ether;
 
   Counters.Counter private currentTokenId;
 
@@ -16,7 +22,13 @@ contract ElementSix is ERC721 {
     baseTokenURI = "";
   }
 
-  function mintTo(address recipient) public returns (uint256) {
+  function mintTo(address recipient) public payable returns (uint256) {
+    uint256 tokenId = currentTokenId.current();
+    // Checks total token supply is not more than total supply limit (say 10,000)
+    require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+    // Contract charges 0.08 ether to call the mintTo() function i.e. Mint Price
+    require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
     currentTokenId.increment();
     uint256 newItemId = currentTokenId.current();
     _safeMint(recipient, newItemId);
@@ -29,7 +41,12 @@ contract ElementSix is ERC721 {
   }
 
   /// @dev Sets the base token URI prefix.
-  function setBaseTokenURI(string memory _baseTokenURI) public {
+  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
     baseTokenURI = _baseTokenURI;
   }
+
+  /// @dev Overridden in order to make it an onlyOwner function
+  function withdrawPayments(address payable payee) public override onlyOwner virtual {
+      super.withdrawPayments(payee);
+  }  
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- require('dotenv').config();
- require("@nomiclabs/hardhat-ethers");
- require("./scripts/deploy.js");
- require("./scripts/mint.js");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+// require('@nomiclabs/hardhat-waffle');
+require("./scripts/deploy.js");
+require("./scripts/mint.js");
+require("@nomiclabs/hardhat-etherscan");
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.1",
@@ -31,6 +34,9 @@ module.exports = {
       chainId: 80001,
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
-    },        
+    },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },  
 }; 
